93601b72f550c12df58bf8ae2a470b8081f2f98c
Abdelouahed <medjoudja@sindibadinternational.net>
2020-12-15 01:24:07 +0100

    screenshots

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,7 +22,6 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:ThemeData.dark(),
       home: Home(),
     );
   }
